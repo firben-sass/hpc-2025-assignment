397d26e6f374e363d80c543e453aff39b913eb7b
magn3144 <magnus@elgaard.com>
2025-01-15 16:41:38 +0100

    Define u and f

--- main.c
+++ main.c
@@ -16,19 +16,6 @@
 
 #define N_DEFAULT 100
 
-void define_u(double *** u, int N)
-{
-    for (int i = 1; i < N+1; i++)
-    {
-        for (int j = 1; j < N+1; j++)
-        {
-            for (int k = 1; k < N+1; k++)
-            {
-                
-            }
-        }
-    }
-}
 
 int main(int argc, char *argv[]) {
 
