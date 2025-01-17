12418498cc02855b8831c25af49bbac53d51b75f
Gladesen <line@gladesen.dk>
2025-01-17 09:53:02 +0100

    Added another version of the Gauss-Seidel (sequential) implementation

---
+++ gauss_seidel_LG.c
@@ -0,0 +1,21 @@
+/* gauss_seidel.c - Poisson problem in 3d
+ *
+ */
+#include <math.h>
+
+double *** gauss_seidel(double ***u, double ***f, int N) {
+    // fill in your code here
+    double factor = 1.0/6.0;
+    double delta = 2.0/(N-1);
+    double delta_2 = delta * delta;
+
+    for (int i = 1; i<N+1; i++) {
+        for (int j = 1; j<N+1; j++) {
+            for (int k = 1; k<N+1; k++) {
+                u[i][j][k] = factor * (u[i-1][j][k] + u[i+1][j][k] + u[i][j-1][k] + u[i][j+1][k] + u[i][j][k-1] + u[i][j][k+1] + delta_2 * f[i][j][k]);
+            }
+        }
+    }
+    return u;
+}
+
