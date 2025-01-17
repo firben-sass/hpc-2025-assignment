09b2616bcdd71eea96fea3142bda922e243afc75
zeynebhadyy <zeynebhady@gmail.com>
2025-01-17 13:01:48 +0100

    Add files via upload

---
+++ gauss_seidel_omp.h
@@ -0,0 +1,10 @@
+/* gauss_seidel_omp.h - Poisson problem
+ *
+ */
+#ifndef _GAUSS_SEIDEL_omp_H
+#define _GAUSS_SEIDEL_omp_H
+
+// define your function prototype here
+double*** gauss_seidel_omp(double ***u, double ***f,int N);
+
+#endif
